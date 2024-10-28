568dff5af7f252056dfe41fe8ac183f62db226dc
strayByte2022 <linhhandsom@gmail.com>
2024-10-28 16:53:31 +0700

    chore: add instructions on settings

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-    std::ifstream inputFile("./test_cases/Sample_test/Test_1/input_matrix.txt");
+    std::ifstream inputFile("./test_cases/Sample_test/Test_2/input_matrix.txt");
     if(!inputFile){
         cerr<<"Unable to open file!";
         return 1;
@@ -27,6 +27,13 @@ int main(){
             settingsLimit -=1;
         }
     }
+
+    // 0: size of image matrix 3 <= n <= 7 -> float
+    // 1: size of kernel matrix 2 <= m <= 4 -> float
+    // 2: size of padding 0 <= p <= 4
+    // 3: the value of stride 1 <= s <= 3
+
+    
 
 
 
