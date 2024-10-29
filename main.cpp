9a633d6fce94e8f99a914a6a2ef25a3bd4018573
strayByte2022 <linhhandsom@gmail.com>
2024-10-29 16:56:55 +0700

    Continuos dev

--- main.cpp
+++ main.cpp
@@ -11,9 +11,9 @@ int main(){
         return 1;
     }
     string content;
-    if(getline(inputFile, content)){
-        cout<<'\n'<<content;
-    }
+    // if(getline(inputFile, content)){
+    //     cout<<'\n'<<content;
+    // }
     inputFile.close(); // close after reading 
 
     
@@ -32,6 +32,43 @@ int main(){
     // 1: size of kernel matrix 2 <= m <= 4 -> float
     // 2: size of padding 0 <= p <= 4
     // 3: the value of stride 1 <= s <= 3
+    
+
+    //check the size
+
+    if(settings[0] < 3 || settings[0] > 7){
+        return 0;
+    }
+
+    if(settings[1] < 2 || settings[1] > 4){
+        return 0;
+    }
+    
+    if(settings[2] < 0 || settings[2] > 4){
+        return 0;
+    }
+
+    if(settings[3] < 1 || settings[1] > 3){
+        return 0;
+    }
+    
+    string rawArray [3];
+    int j = 0;
+
+    string a;
+
+    while(inputFile >> a){
+        rawArray[j] = a;
+        j++;
+    }
+
+    for(int i =0; i < 3; i++){
+        cout<<"Hello: "<<rawArray[i];
+    }
+
+
+
+
 
     
 
